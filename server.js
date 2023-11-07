6e3be6c3ced1ffd5c2c10f96881fca773adada39
Cfeitz99 <147496459+Cfeitz99@users.noreply.github.com>
2023-11-07 15:11:48 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve the static HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Endpoint to create payment and redirect user
+app.get('/create-payment', async (req, res) => {
+  try {
+    // Replace 'YOUR_ZAPIER_WEBHOOK_URL' with the webhook URL provided by Zapier
+    const zapierResponse = await axios.post('YOUR_ZAPIER_WEBHOOK_URL', {});
+
+    // Zapier should respond with the payment URL in the response body
+    if (zapierResponse.data && zapierResponse.data.paymentUrl) {
+      return res.redirect(zapierResponse.data.paymentUrl);
+    }
+
+    throw new Error('Payment URL not provided by Zapier');
+  } catch (error) {
+    return res.status(500).send('Error creating payment.');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
