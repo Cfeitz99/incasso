066bb736f973814b9ec1e835e82f03dc2d44f774
Cfeitz99 <147496459+Cfeitz99@users.noreply.github.com>
2023-11-07 15:45:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,29 +10,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Endpoint to create payment and redirect user
 app.get('/create-payment', async (req, res) => {
-  const contactId = req.query.contact_id; // Extract contact_id from the query parameters
-
-  try {
-    if (!contactId) {
-      throw new Error('No contact ID provided');
-    }
-
-    // Include the contact ID in the data sent to Zapier
-    const zapierResponse = await axios.post('https://hooks.zapier.com/hooks/catch/16510018/38cygnn/', {
-      contactId: contactId
-    });
-
-    // Zapier should respond with the payment URL in the response body
-    if (zapierResponse.data && zapierResponse.data.paymentUrl) {
-      return res.redirect(zapierResponse.data.paymentUrl);
-    }
-
-    throw new Error('Payment URL not provided by Zapier');
-  } catch (error) {
-    console.error(error.message);
-    return res.status(500).send('Error creating payment. ' + error.message);
-  }
+  res.send('Create payment endpoint is working');
 });
+
 
