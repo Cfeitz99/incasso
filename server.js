60d66a988690ba07a518b5f2c578061afbf8bda2
Cfeitz99 <147496459+Cfeitz99@users.noreply.github.com>
2023-11-07 17:32:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,13 +21,20 @@ let paymentUrls = {};
 // Endpoint to receive the payment URL from Zapier
 app.post('/receive-payment-url', express.json(), (req, res) => {
   const { paymentUrl, contactId } = req.body;
-  console.log('Received payment URL:', paymentUrl);
+  console.log(`Received payment URL for contactId ${contactId}: ${paymentUrl}`);
   
+  if (!contactId || !paymentUrl) {
+    console.error('Invalid data received for payment URL');
+    return res.status(400).json({ success: false, message: 'Invalid data received' });
+  }
+
   // Store the payment URL using the contact ID as a key
   paymentUrls[contactId] = paymentUrl;
   
+  console.log(`Payment URL stored for contactId ${contactId}`);
   res.status(200).json({ success: true });
 });
+
 
 // Endpoint to create payment and initiate the process
 app.get('/create-payment', async (req, res) => {
@@ -66,23 +73,21 @@ app.get('/create-payment', async (req, res) => {
 });
 
 
-// Endpoint for the frontend to poll for the payment URL availability
 app.get('/check-payment-url', (req, res) => {
   const contactId = req.query.contact_id;
+  console.log(`Checking payment URL for contactId ${contactId}:`, paymentUrls[contactId]);
   
-  // Retrieve the payment URL using the contact ID
   const paymentUrl = paymentUrls[contactId];
   
-  console.log(`Checking payment URL for contactId ${contactId}: `, paymentUrl);
-  
   if (paymentUrl) {
-    console.log(`Sending available payment URL for contactId ${contactId}`);
+    console.log(`Payment URL available for contactId ${contactId}`);
     res.json({ available: true, paymentUrl: paymentUrl });
   } else {
     console.log(`No payment URL available for contactId ${contactId}`);
     res.json({ available: false });
   }
 });
+
 
 
 app.listen(port, () => {
