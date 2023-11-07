5bea25f285ab4d269e91229823dfb21926482037
Cfeitz99 <147496459+Cfeitz99@users.noreply.github.com>
2023-11-07 16:03:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,20 +13,27 @@ app.get('/', (req, res) => {
 // Endpoint to create payment and redirect user
 app.get('/create-payment', async (req, res) => {
   try {
-    // Replace 'YOUR_ZAPIER_WEBHOOK_URL' with the webhook URL provided by Zapier
-    const zapierResponse = await axios.post('https://hooks.zapier.com/hooks/catch/16510018/38cygnn/', {});
+    // Insert the actual Zapier webhook URL here
+    const zapierWebhookUrl = 'https://hooks.zapier.com/hooks/catch/16510018/38cygnn/';
+
+    // Make a POST request to the Zapier webhook
+    const zapierResponse = await axios.post(zapierWebhookUrl, {});
 
     // Zapier should respond with the payment URL in the response body
     if (zapierResponse.data && zapierResponse.data.paymentUrl) {
+      // Redirect the user to the payment URL provided by Zapier
       return res.redirect(zapierResponse.data.paymentUrl);
+    } else {
+      // If the payment URL is not provided, throw an error
+      throw new Error('Payment URL not provided by Zapier');
     }
-
-    throw new Error('Payment URL not provided by Zapier');
   } catch (error) {
-    return res.status(500).send('Error creating payment.');
+    // If an error occurs, send a 500 status code and the error message
+    return res.status(500).send(`Error creating payment: ${error.message}`);
   }
 });
 
+// Start the server on the specified port
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
